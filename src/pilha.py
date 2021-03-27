e9d5b09d64ccf2e9159c4cad690b4b0057f97cce
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-26 22:06:11 -0300

    Refactoring Stack

--- src/pilha.py
+++ src/pilha.py
@@ -1,24 +1,53 @@
+import random
+
 class Stack: 
 
     def __init__(self):
-        self.stack = []
+        self.__stack = []
+        self.__len_stack = 0
 
     def push(self, e):
-        self.stack.append(e)
+        self.__stack.append(e)
+        self.__len_stack += 1
 
     def pop(self):
         if not self.empty():
-            self.stack.pop(len(self.stack) - 1)
+            self.__stack.pop(self.__len_stack - 1)
+            self.__len_stack -= 1
 
     def top(self):
         if not self.empty():
-            return self.stack[-1]
-        return None
+            return self.__stack[-1]
+        return None 
 
     def empty(self):
-        if (len(self.stack) == 0):
+        if self.__len_stack == 0:
             return True
         return False
 
     def length(self):
-        return len(self.stack)
+        return self.__len_stack
+    
+    def show(self):
+        print('Stack: {}'.format(self.__stack))
+
+
+def main():
+
+    stack = Stack()
+
+    for _ in range(0, 10):
+        stack.push(random.randint(10, 99))
+
+    stack.length()
+    stack.show()
+
+    stack.pop()
+    stack.pop()
+
+    stack.length()
+    stack.show()
+
+
+if __name__ == '__main__':
+    main()
